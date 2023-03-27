4233213b91c7c5cf30fa58aed619c26abe1b7a89
michael-junior <michaeljunioroliveira@gmail.com>
2023-03-27 10:17:21 -0300

    Implementação para primeira versão do modulo

--- src/main/scala/CompareDocsColls.scala
+++ src/main/scala/CompareDocsColls.scala
@@ -1,17 +1,19 @@
 import mongodb.MongoExport
+import org.bson.BsonValue
 import org.json4s.DefaultFormats
 import org.json4s.native.Json
-import org.mongodb.scala.bson.BsonValue
+import org.mongodb.scala.bson.{BsonArray, BsonString}
 import org.mongodb.scala.bson.collection.immutable.Document
 
 import java.util.Date
-import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
+import scala.jdk.CollectionConverters._
 import scala.util.{Failure, Success, Try}
 
 case class InputParameters_CompMongoCol(database_from1: String,
                                         collection_from1: String,
                                         collection_from2: String,
                                         collection_out: String,
+                                        idField: String,
                                         database_from2: Option[String],
                                         database_out: Option[String],
                                         host_from1: Option[String],
@@ -33,39 +35,108 @@ case class InputParameters_CompMongoCol(database_from1: String,
                                         append: Boolean)
 
 
-class CompareDocsColls {
+class CompMongoCol {
 
-  private def compareDocsColls(parameters: InputParameters_CompMongoCol): Try[Unit] = {
+  private def compMongoCol(parameters: InputParameters_CompMongoCol): Try[Unit] = {
     Try {
-      val mongo_from1: MongoExport = new MongoExport(parameters.database_from1, parameters.collection_from1, parameters.host_from1, parameters.port_from1, parameters.user_from1, parameters.password_from1, parameters.total, parameters.append)
-      val mongo_from2: MongoExport = new MongoExport(parameters.database_from2.getOrElse(""), parameters.collection_from2, parameters.host_from2, parameters.port_from2, parameters.user_from2, parameters.password_from2, parameters.total, parameters.append)
-      val mongo_out: MongoExport = new MongoExport(parameters.database_out.getOrElse(""), parameters.collection_out, parameters.host_out, parameters.port_out, parameters.user_out, parameters.password_out, parameters.total, parameters.append)
+      val mongo_instance1: MongoExport = new MongoExport(parameters.database_from1, parameters.collection_from1, parameters.host_from1, parameters.port_from1, parameters.user_from1, parameters.password_from1, parameters.total, parameters.append)
+      val mongo_instance2: MongoExport = new MongoExport(parameters.database_from2.getOrElse(""), parameters.collection_from2, parameters.host_from2, parameters.port_from2, parameters.user_from2, parameters.password_from2, parameters.total, parameters.append)
+      val mongo_instanceOut: MongoExport = new MongoExport(parameters.database_out.getOrElse(""), parameters.collection_out, parameters.host_out, parameters.port_out, parameters.user_out, parameters.password_out, parameters.total, parameters.append)
 
-      val listDoc1: Seq[Document] = mongo_from1.findAll
-      val listDoc2: Seq[Document] = mongo_from2.findAll
+      val docs_instance1: Seq[Document] = mongo_instance1.findAll
+      val docs_instance2: Seq[Document] = mongo_instance2.findAll
 
-      val noCompFields: Array[String] = parameters.noCompFields.getOrElse("").split(",")
-      val takeFields: Array[String] = parameters.takeFields.getOrElse("").split(",")
-
-      val docsDiff1: Seq[Document] = listDoc1.map(doc1 => doc1.filter(field1 => if (!takeFields.contains(field1._1)) !listDoc2.exists(doc2 => doc2.exists(field2 => field1.equals(field2))) else true))
-      val docsNonEmpty: Seq[Document] = docsDiff1.dropWhile(f => f.isEmpty)
-      val docsWithNoCompFields: Seq[Document] = docsNonEmpty.map(doc => doc.filterNot(field => noCompFields.contains(field._1)))
-
-      docsWithNoCompFields.foreach(f => mongo_out.insertDocument(Json(DefaultFormats).write(f.toMap.map(f => (f._1, getValue(f._2))))))
+      val documentsCompared: Seq[Array[(String, Array[BsonValue])]] = compareDocuments(docs_instance1, docs_instance2, parameters.idField, parameters.noCompFields, parameters.takeFields)
+      //val documentsComparedSortedByKey = documentsCompared.sortBy(_.head._1).map(_.sortBy(_._1))
+      val listJson: Seq[String] = documentsCompared.map(f => Json(DefaultFormats).write(f.toMap.map(f => getValue(f._1, f._2))))
+      listJson.sorted.foreach(mongo_instanceOut.insertDocument)
     }
   }
 
-  private def getValue(f: BsonValue): AnyRef = {
 
-    f match {
-      case f if f.isObjectId => f.asObjectId().getValue.toString
-      case f if f.isArray => f.asArray().getValues.map(f => f.asString().getValue).toArray
-      case _ => f.asString().getValue
+  private def compareDocuments(docs_list1: Seq[Document], docs_list2: Seq[Document], identifierField: String, noCompFields: Option[String], takeFields: Option[String]):  Seq[Array[(String, Array[BsonValue])]] = {
+
+    val noCompFieldsParam: Array[String] = noCompFields.getOrElse("_id").split(",") :+ "_id"
+    val takeFieldsParam: Array[String] = takeFields.getOrElse(identifierField).split(",") :+ identifierField
+
+    val docsListOneDiffDocsListTwo: Seq[Document] = compareDocumentsBetweenLists(docs_list1, docs_list2, takeFieldsParam)
+    val docsListTwoDiffDocsListOne: Seq[Document] = compareDocumentsBetweenLists(docs_list2, docs_list1, takeFieldsParam)
+
+    val docsListOneValid: Seq[Document] = deleteDocumentsWithUnnecessaryFields(docsListOneDiffDocsListTwo, identifierField)
+    val listDocsOneWithoutMongoId: Seq[Document] = docsListOneValid.map(doc => doc.filterNot(field => noCompFieldsParam.contains(field._1)))
+
+    val docsListTwoValid: Seq[Document] = deleteDocumentsWithUnnecessaryFields(docsListTwoDiffDocsListOne, identifierField)
+    val listDocsTwoWithoutMongoId: Seq[Document] = docsListTwoValid.map(doc => doc.filterNot(field => noCompFieldsParam.contains(field._1)))
+
+    val listDocsOneFieldsRename: Map[String, Document] = renameFieldsWith_InstanceNumber(listDocsOneWithoutMongoId, identifierField, "1").map(doc => (doc.getString(identifierField), doc)).toMap
+    val listDocsOneAndTwoComparedUnified: Map[String, Document] = mergeFieldsInDocumentsById(identifierField, listDocsTwoWithoutMongoId, listDocsOneFieldsRename)
+
+    val updatedList: Seq[Document] = listDocsOneAndTwoComparedUnified.values.toList
+
+    defineArrayValuePosition(updatedList)
+  }
+
+
+  private def mergeFieldsInDocumentsById(identifierField: String, listDocsTwoWithoutMongoId: Seq[Document], listDocsOneFieldsRename: Map[String, Document]): Map[String, Document] = {
+    renameFieldsWith_InstanceNumber(listDocsTwoWithoutMongoId, identifierField, "2").foldLeft(listDocsOneFieldsRename) { (acc, doc) =>
+      acc.get(doc.getString(identifierField)) match {
+        case Some(existingDoc) => acc.updated(doc.getString(identifierField), existingDoc ++ doc)
+        case None => acc
+      }
+    }
+  }
+
+  private def deleteDocumentsWithUnnecessaryFields(docsListOneDiffDocsListTwo: Seq[Document], identifierField: String): Seq[Document] = {
+    docsListOneDiffDocsListTwo.filterNot(f => f.isEmpty || f.contains("_id") && f.size == 1 || f.contains("_id") && f.contains(identifierField) && f.size <= 2)
+  }
+
+  private def compareDocumentsBetweenLists(list1: Seq[Document], list2: Seq[Document], takeFieldsParam: Array[String]): Seq[Document] = {
+    list1.map(doc1 => doc1.filter{
+      field1 =>
+        if (!takeFieldsParam.contains(field1._1))
+          !list2.exists(doc2 => doc2.exists(field2 => field1.equals(field2)))
+        else true
+    })
+  }
+
+  private def renameFieldsWith_InstanceNumber(list: Seq[Document], idField: String, numColl: String) : Seq[Document]= {
+
+    val renamedList = list.map { originalDoc =>
+      Document(
+        originalDoc.map { case (oldKey, value) =>
+          val newKey = oldKey match {
+            case key if key == idField || key == "_id" => oldKey
+            case _ => oldKey.concat(s" _$numColl")
+          }
+          newKey -> value
+        }.toSeq: _*
+      )
+    }
+    renamedList
+  }
+
+  private def defineArrayValuePosition(updatedList: Seq[Document]) : Seq[Array[(String, Array[BsonValue])]] = {
+
+    updatedList.map(document => document.toArray.map(field => (field._1,
+      if (field._1.endsWith(" _1")) {
+        Array(field._2, if (field._2.isArray) BsonArray("") else BsonString(""))
+      } else {
+        Array(if (field._2.isArray) BsonArray("") else BsonString(""), field._2)
+      }
+    )))
+  }
+
+
+  private def getValue(key: String, value: Array[BsonValue]): (String, AnyRef) = {
+
+    key match {
+      case key if value.exists(_.isArray) => (key, value.map(_.asArray().getValues.asScala.map(_.asString().getValue)))
+      case key if value.exists(_.isString) => (key, value.map(_.asString().getValue))
     }
   }
 }
 
-object CompareDocsColls {
+object CompMongoCol {
 
   private def usage(): Unit = {
     System.err.println("\nEnter the input parameters:")
@@ -73,23 +144,24 @@ object CompareDocsColls {
     System.err.println("-collection_from1=<name>              ~ required     - MongoDB database collection one name")
     System.err.println("-collection_from2=<name>              ~ required     - MongoDB database collection two name")
     System.err.println("-collection_out=<name>                ~ required     - MongoDB database collection out name")
+    System.err.println("-idField=<str>                        ~ required     - Identifier field for document comparison")
     System.err.println("[-database_from2=<number>]            - MongoDB database two name")
     System.err.println("[-database_out=<name>])               - MongoDB database out name")
-    System.err.println("[-host_from1=<pwd>]                   - MongoDB server name. Default value is 'localhost'")
-    System.err.println("[-port_from1=<str>]                   - MongoDB server port number. Default value is 27017")
-    System.err.println("[-host_from2=<path>]                  - MongoDB server name. Default value is 'localhost'")
-    System.err.println("[-port_from2=<str>]                   - MongoDB server port number. Default value is 27017")
-    System.err.println("[-host_out]                           - MongoDB server name. Default value is 'localhost'")
-    System.err.println("[-port_out]                           - MongoDB server port number. Default value is 27017")
-    System.err.println("[-user_from1]                         - MongoDB user name")
-    System.err.println("[-password_from1]                     - MongoDB user password")
-    System.err.println("[-user_from2]                         - MongoDB user name")
-    System.err.println("[-password_from2]                     - MongoDB user password")
-    System.err.println("[-user_out]                           - MongoDB user name")
-    System.err.println("[-password_out]                       - MongoDB user password")
+    System.err.println("[-host_from1=<pwd>]                   - MongoDB server name one. Default value is 'localhost'")
+    System.err.println("[-port_from1=<str>]                   - MongoDB server port number one. Default value is 27017")
+    System.err.println("[-host_from2=<path>]                  - MongoDB server name two. Default value is 'localhost'")
+    System.err.println("[-port_from2=<str>]                   - MongoDB server port number two. Default value is 27017")
+    System.err.println("[-host_out]                           - MongoDB server name out. Default value is 'localhost'")
+    System.err.println("[-port_out]                           - MongoDB server port numbervout. Default value is 27017")
+    System.err.println("[-user_from1]                         - MongoDB user name one")
+    System.err.println("[-password_from1]                     - MongoDB user password one")
+    System.err.println("[-user_from2]                         - MongoDB user name two")
+    System.err.println("[-password_from2]                     - MongoDB user password two")
+    System.err.println("[-user_out]                           - MongoDB user name out")
+    System.err.println("[-password_out]                       - MongoDB user password out")
     System.err.println("[-total]                              - If present, take total documents")
-    System.err.println("[-noCompFields]                       - Conversion field string to array")
-    System.err.println("[-takeFields]                         - Conversion field string to array")
+    System.err.println("[-noCompFields]                       - Field(s) that will not be compared")
+    System.err.println("[-takeFields]                         - Fields that must be present even if not being compared")
     System.err.println("[-noUpDate]                           - If present, it will not create de update date field (_updd), otherwise it will be created")
     System.err.println("[-append]                             - If absent, it will append documents into the MongoDB database, otherwise the database will be previously erased")
     System.exit(1)
@@ -111,7 +183,7 @@ object CompareDocsColls {
       usage()
     }
 
-    val listInputParam: List[String] = List("database_from1", "collection_from1", "collection_from2", "collection_out", "database_from2", "database_out", "host_from1",
+    val listInputParam: List[String] = List("database_from1", "collection_from1", "collection_from2", "collection_out", "idField", "database_from2", "database_out", "host_from1",
       "port_from1", "host_from2", "port_from2", "host_out", "port_out", "user_from1", "password_from1", "user_from2", "password_from2", "user_out", "password_out",
       "total", "noCompFields", "takeFields", "noUpdDate", "append")
 
@@ -125,6 +197,7 @@ object CompareDocsColls {
     val collection_from1: String = parameters("collection_from1")
     val collection_from2: String = parameters("collection_from2")
     val collection_out: String = parameters("collection_out")
+    val idField: String = parameters("idField")
     val database_from2: Option[String] = parameters.get("database_from2")
     val database_out: Option[String] = parameters.get("database_out")
     val host_from1: Option[String] = parameters.get("host_from1")
@@ -146,10 +219,10 @@ object CompareDocsColls {
     val append: Boolean = parameters.contains("append")
 
     val params: InputParameters_CompMongoCol = InputParameters_CompMongoCol(database_from1, collection_from1, collection_from2,
-      collection_out, database_from2, database_out, host_from1, port_from1, host_from2, port_from2, host_out, port_out,
+      collection_out, idField, database_from2, database_out, host_from1, port_from1, host_from2, port_from2, host_out, port_out,
       user_from1, password_from1, user_from2, password_from2, user_out, password_out, total, noCompFields, takeFields, noUpDate, append)
     val startDate: Date = new Date()
-    (new CompareDocsColls).compareDocsColls(params) match {
+    (new CompMongoCol).compMongoCol(params) match {
       case Success(_) =>
         println(timeAtProcessing(startDate))
         System.exit(0)
